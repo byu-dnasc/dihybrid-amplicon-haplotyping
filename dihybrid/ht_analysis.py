42d83e35e14a769fe28fb2451142c3cfa44354e5
Ammon Knaupp <adknaupp@gmail.com>
2024-07-17 20:22:53 -0700

    Catch and report "invalid" executions, strip newlines from guide FASTA.

--- dihybrid/ht_analysis.py
+++ dihybrid/ht_analysis.py
@@ -14,7 +14,7 @@ HAPLOTYPES = parameters.get['haplotypes']
 GUIDE_FILENAME = parameters.get['guide']
 with open(f'guides/{GUIDE_FILENAME}', 'r') as f:
     lines = f.readlines()
-    REF_SEQ = ''.join(lines[1:])
+    REF_SEQ = ''.join([l.strip() for l in lines[1:]])
 
 def _get_alignment(query):
     alignment = ALIGNER.align(REF_SEQ, query)[0]
@@ -30,35 +30,55 @@ def _get_haplotype(seq):
     snp_1, snp_2 = alignment[1,pos_1], alignment[1,pos_2]
     return (snp_1+snp_2).lower()
 
-def main():
-    assert os.path.isdir('execution'), 'No pbaa execution directory found. Run pbaa first.'
-    rows = []
-    cluster_rows = []
-    for sample_name in [fn for fn in os.listdir('execution')]:
-        ht_counts = {ht: 0 for ht in HAPLOTYPES}
-        for cluster_name, seq, read_count, freq, possibly_chimeric in fromfile.get_clusters(sample_name):
-            if freq < 0.05:
-                continue
-            haplotype = _get_haplotype(seq)
-            assert haplotype in HAPLOTYPES, f'haplotype {haplotype} not among possible haplotypes {HAPLOTYPES}'
-            ht_counts[haplotype] += read_count
-            cluster_rows.append((sample_name, cluster_name, haplotype))
-        total_reads = fromfile.get_total_num_reads(sample_name)
-        reads_passing_qc = fromfile.get_num_input_reads(sample_name)
-        rows.append((sample_name, total_reads, reads_passing_qc) + tuple(ht_counts[ht] for ht in HAPLOTYPES))
+def _execution_valid(sample_name):
+    '''Identify subdirs of execution/ which contain a FASTA of passed
+    cluster sequences.'''
+    filenames = os.listdir(f'execution/{sample_name}')
+    if f'{sample_name}_passed_cluster_sequences.fasta' in filenames:
+        return True
+    return False
 
+def write_clusters(cluster_rows):
     with open('clusters_by_haplotype.json', 'w') as f:
         header = ('sample_name', 'cluster_name', 'haplotype')
         f.write(','.join(header) + '\n')
         for line in cluster_rows:
             f.write(','.join(line) + '\n')
 
+def write_csv(rows):
     # write the haplotype counts to a file
     with open('haplotype_counts.tsv', 'w') as f:
         header = ('sample_name', 'total_reads', 'reads_passing_qc') + tuple('haplotype_' + ht + '_reads' for ht in HAPLOTYPES)
         f.write('\t'.join(header) + '\n')
         for row in rows:
             f.write('\t'.join(str(x) for x in row) + '\n')
+
+def main():
+    assert os.path.isdir('execution'), 'No pbaa execution directory found. Run pbaa first.'
+    incomplete_executions = []
+    rows = []
+    cluster_rows = []
+    for sample_name in os.listdir('execution'):
+        if not _execution_valid(sample_name):
+            incomplete_executions.append(sample_name)
+            continue
+        ht_counts = {ht: 0 for ht in HAPLOTYPES}
+        for cluster_name, seq, read_count, freq, possibly_chimeric in fromfile.get_clusters(sample_name):
+            if freq < 0.05:
+                continue
+            haplotype = _get_haplotype(seq)
+            assert haplotype in HAPLOTYPES, f'haplotype {haplotype} not among possible haplotypes {HAPLOTYPES} ({sample_name=}, {cluster_name=})'
+            ht_counts[haplotype] += read_count
+            cluster_rows.append((sample_name, cluster_name, haplotype))
+        total_reads = fromfile.get_total_num_reads(sample_name)
+        reads_passing_qc = fromfile.get_num_input_reads(sample_name)
+        rows.append((sample_name, total_reads, reads_passing_qc) + tuple(ht_counts[ht] for ht in HAPLOTYPES))
+
+    print(f'pbAA execution(s) for {len(incomplete_executions)}',
+          f'samples are incomplete: {' '.join(incomplete_executions)}')
+    
+    write_clusters(cluster_rows)
+    write_csv(rows)
 
 if __name__ == '__main__':
     main()
